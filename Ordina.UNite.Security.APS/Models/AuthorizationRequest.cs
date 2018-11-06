6e84638c6d62c0cda817e50f8e411c7f114e02e9
Kevin Bosteels <Kevin.Bosteels@ordina.be>
2018-11-07 00:56:49 +0100

    move models

---
+++ Ordina.UNite.Security.APS/Models/AuthorizationRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordina.UNite.Security.APS.Controllers
+{
+    public class AuthorizationRequest
+    {
+        public string ResourceType { get; set; }
+        public string Action { get; set; }
+        public object Resource { get; set; }
+    }
+}
---
+++ Ordina.UNite.Security.APS/Models/AuthorizationResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Ordina.UNite.Security.APS.Controllers
+{
+    public class AuthorizationResponse
+    {
+        public string Action { get; set; }
+        public bool HasAccess { get; set; }
+    }
+}
